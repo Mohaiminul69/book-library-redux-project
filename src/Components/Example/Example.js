125dc9581eb72511a195ef32e725495384131945
mohaiminul69 <mohaiminul69@gmail.com>
2022-01-02 19:08:24 +0600

    routes created

---
+++ src/Components/Example/Example.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Example = () => {
+  return (
+    <div>
+      <h1>
+        Create a Different folder and the work on it. and push to your branch.
+      </h1>
+    </div>
+  );
+};
+
+export default Example;
