d5625fd25c62687b1a6648e77bafc1e44676ad30
mohaiminul69 <mohaiminul69@gmail.com>
2022-01-05 14:50:41 +0600

    fetching data from backend

--- src/Components/Books/DiscoverBooks/DiscoverBooks.js
+++ src/Components/Books/DiscoverBooks/DiscoverBooks.js
@@ -1,32 +1,29 @@
-import React, { useEffect } from 'react';
-import SingleDiscoverBook from '../SingleDiscoverBook/SingleDiscoverBook';
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import { fetchBooks } from "../../../redux/slices/bookSlice";
+import React, { useEffect } from "react";
+import SingleDiscoverBook from "../SingleDiscoverBook/SingleDiscoverBook";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { fetchBooks } from "./../../../redux/slices/bookSlice";
 
 const DiscoverBooks = () => {
-    const books = useSelector((state) => state.books.discover);
-    // const dispatch = useDispatch();
+  const books = useSelector((state) => state.books.discover);
+  const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     dispatch(fetchBooks());
-    // }, [dispatch]);
-    
-    return (
-        <div>
-            <div className='px-2'>
-                <h3>Discover Books</h3>
-                <div className='py-2'>
-                    {
-                        books.map(book => <SingleDiscoverBook
-                            key={book.id}
-                            book={book}
-                        />)
-                    }
-                </div>
-            </div>
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+
+  return (
+    <div>
+      <div className="px-2">
+        <h3>Discover Books</h3>
+        <div className="py-2">
+          {books.map((book) => (
+            <SingleDiscoverBook key={book.id} book={book} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default DiscoverBooks;
